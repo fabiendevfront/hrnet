542209924f2e92e50c347039f3e1299cc5365251
Fabien Dev <fabiendevfront@gmail.com>
2023-02-23 16:49:52 +0100

    Create header component, design & responsive

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,7 +1,24 @@
+import logo from "../assets/logo.png";
+import formIcon from "../assets/icons/form.svg";
+import listIcon from "../assets/icons/list.svg";
+import { NavLink, Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header className="header">
-            <h2>Header</h2>
+            <Link to="/" className="header__link">
+                <h1>
+                    <img src={logo} className="header__logo" alt="Logo du site Hrnet" />
+                </h1>
+            </Link>
+            <nav className="header__nav">
+                <NavLink to="/create-employee" className="header__nav-link">
+                    <img className="header__icon" src={formIcon} alt="form icon" />Create employee
+                </NavLink>
+                <NavLink to="/employee-list" className="header__nav-link">
+                    <img className="header__icon" src={listIcon} alt="list icon" />Employee list
+                </NavLink>
+            </nav>
         </header>
     );
 };
